a014c39cdd33569c1df4b91a475f3138e0ca3ec6
Renato <rsalhuana@gmail.com>
2017-07-13 23:06:10 -0500

    asignacion encuestadores
    asignacion encuestadores

--- getUbigeoData.php
+++ getUbigeoData.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'dbconnect.php';
+session_start();
 
 if(isset($_POST["ciudad_id"]) && !empty($_POST["ciudad_id"]) && $_POST["ciudad_id"] != 'CIU11'){
     $locales = mysql_query("select * from Local where idCiudad = '" .  $_POST['ciudad_id'] . "' Order by Local ASC");
@@ -19,7 +20,7 @@ if(isset($_POST["ciudad_id"]) && !empty($_POST["ciudad_id"]) && $_POST["ciudad_i
 }
 if(isset($_POST["distrito_id"]) && !empty($_POST["distrito_id"])){
     //$locales = mysql_query("Select * FROM Local l Join Asignacion a on l.idLocal = a.idLocal WHERE idDistrito = '" . $_POST['distrito_id'] . "' AND a.idUsuario = '". $_SESSION['userid'] ."' AND a.idSemana = '" . $_POST['semana'] . "' Order by Local ASC");
-    $locales = mysql_query("Select * FROM Local l Join Asignacion a on l.idLocal = a.idLocal WHERE idDistrito = '" . $_POST['distrito_id'] . "' AND a.idUsuario = '". $_SESSION['userid'] ."' Order by Local ASC");
+    $locales = mysql_query("Select * FROM Local l Join Asignacion a on l.idLocal = a.idLocal WHERE idDistrito = '" . $_POST['distrito_id'] . "' AND a.idUsuario = ". $_SESSION['userid'] ." Order by Local ASC");
     echo '<option value="">Seleccione Local</option>';
     while ($row = mysql_fetch_assoc($locales)) {
         echo '<option value="'.$row['idLocal'].'">'.$row['Local'].'</option>';
