a0c430bac77a3490664853d18fe0b4181f9a9ca1
Renato <rsalhuana@gmail.com>
2017-07-15 09:29:08 -0500

    encuesta script js

---
+++ encuesta_script.js
@@ -0,0 +1,146 @@
+$(document).ready(function () {
+    //jQuery.validator.format("{0} debe tener un valor");
+    
+    $('#frmenc').validate({ // initialize the plugin
+        //set this to false if you don't what to set focus on the first invalid input
+        focusInvalid: false,
+        //by default validation will run on input keyup and focusout
+        //set this to false to validate on submit only
+        onkeyup: false,
+        onfocusout: false,
+        highlight: function (element, errorClass) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element, errorClass) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        //by default the error elements is a <label>
+        errorElement: "div",
+        //place all errors in a <div id="errors"> element
+        errorPlacement: function(error, element) {
+            error.appendTo("div#errors");
+        }, 
+        rules: {
+            Foto1: {
+                required: {
+                    depends: function (element) {
+                        return ($('#frmenc select[name="Distribuidor1"]').val() != ''
+                                ||$("#MontoBoleta1").is(":filled"));//return $("#Distribuidor1").val().length > 0;
+                    }
+                }
+            },
+            MontoBoleta1: {
+                required: {
+                    depends: function (element) {
+                        return ($('#frmenc select[name="Distribuidor1"]').val() != ''
+                                || $("#Foto1").is(":filled")); 
+                        //$("#Foto1").is(":filled");//return $("#Distribuidor1").val().length > 0;
+                    }
+                }
+            },
+            Distribuidor1: {
+                required: {
+                    depends: function (element) {
+                        return ($("#MontoBoleta1").is(":filled") 
+                            ||  $("#Foto1").is(":filled"));
+                    }
+                }
+            }
+        },
+            messages: {
+            Foto1: {
+                required: "- Foto 1: Agregue la foto correspondiente."
+            },
+            MontoBoleta1: {
+                required: "- Foto 1: Indique el monto de la boleta."
+            },
+            Distribuidor1:{
+                required: "- Foto 1: Indique el distribuidor de la boleta."
+            }
+        }
+        /*errorPlacement: function(errorMap, errorList) {
+            $.each(this.successList, function(index, value) {
+                return $(value).popover("hide");
+            });
+            return $.each(errorList, function(index, value) {
+                var _popover;
+                _popover = $(value.element).popover({
+                    trigger: "manual",
+                    placement: "auto",
+                    content: value.message,
+                    template: "<div class=\"popover\"><div class=\"arrow\"></div><div class=\"popover-inner\"><div class=\"popover-content\"><p></p></div></div></div>"
+                });
+                _popover.data("bs.popover").options.content = value.message;
+                return $(value.element).popover("show");
+            });
+        }*/
+    });
+
+    $(".input-file-img").fileinput({
+        'showUpload': false
+    });
+    $("input[name='Colgante'],input[name='Jalavista'],input[name='TachoDispensador'],input[name='BannerToldo']").click(function() {
+        var isChecked = false;
+        $("input[name='Colgante'],input[name='Jalavista'],input[name='TachoDispensador'],input[name='BannerToldo']").each(function() {
+            if($(this).is(':checked') && $(this).val() == 'Si') {
+                isChecked = true;
+            }
+        });
+        if (isChecked == true) {
+            $('#MaterialPOP_Si').prop("checked", true);
+        } else {
+            $('#MaterialPOP_No').prop("checked", true);
+        }
+    });
+    /*$("input[name$='MaterialPOP']").click(function() {
+        var answer = $(this).val();
+        
+        if(answer == "Si"){
+            $("#divMaterialPOP").show();
+        }else{
+            $("#divMaterialPOP").hide();
+        }
+        
+    });*/
+
+    $('.panel-collapse').each(function() {
+        $(this).addClass('in');
+    });
+
+    /* open panel where we found the first error */
+    if($('.has-error')[0]) {
+        var errorFound = false;
+        if(!$('#collapseTwo .has-error')[0]) {
+            $('#collapseTwo').removeClass('in');
+        }
+        // open first error panel
+        $('.panel-collapse').each(function() {
+            if($(this).find('.has-error')[0] && errorFound === false) {
+                $(this).addClass('in');
+                errorFound = true;
+            }
+        });
+    }
+
+    $('.onlyDecimal').keydown(function (event) {
+        if (event.shiftKey == true) {
+            event.preventDefault();
+        }
+        if ((event.keyCode >= 48 && event.keyCode <= 57) ||
+            (event.keyCode >= 96 && event.keyCode <= 105) ||
+            event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 37 ||
+            event.keyCode == 39 || event.keyCode == 46 || event.keyCode == 190) {
+        } else {
+            event.preventDefault();
+        }
+        if($(this).val().indexOf('.') !== -1 && event.keyCode == 190) {
+            event.preventDefault();
+        }
+        var decimal = $(this).val().split(".")[1];
+        if (typeof decimal != 'undefined') {
+            if (decimal.length > 1 && event.keyCode != 8 && event.keyCode != 46) {
+                event.preventDefault();
+            }
+        }
+    });
+});
